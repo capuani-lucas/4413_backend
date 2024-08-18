f349f228fb487c7ede32c6c6e845a25fdcd34ce4
capuani-lucas <77223333+capuani-lucas@users.noreply.github.com>
2024-08-18 01:15:28 -0400

    Update settings.py

--- backend_4413/settings.py
+++ backend_4413/settings.py
@@ -24,13 +24,11 @@ env = environ.Env(
 # Take environment variables from .env file
 
 # Env from Railway
-
-if not env("RAILWAY_PROJECT_ID", default=None):
-    environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+if not env("RAILWAY_PROJECT_ID", default=None):
+    environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
